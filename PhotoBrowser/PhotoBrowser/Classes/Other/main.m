5cfe4f9224eb3b1a64f2e87d740178c5cfbfa0bc
CoderQingYan <ZQY@520it.com>
2016-04-29 01:06:39 +0800

    初始化项目
    初始化项目 添加cocopods

---
+++ PhotoBrowser/PhotoBrowser/Classes/Other/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhotoBrowser
+//
+//  Created by 赵清岩 on 16/4/29.
+//  Copyright © 2016年 QingYan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
